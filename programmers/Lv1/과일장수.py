2d166bb013d73d934aba749332c5e9386fea9d4c
kaydenna92 <kaydenna92@gmail.com>
2023-04-06 22:58:27 +0900

    0406 ps solve

--- programmers/Lv1/과일장수.py
+++ programmers/Lv1/과일장수.py
@@ -1,20 +1,15 @@
 def solution(k, m, score):
     answer = 0
     score.sort(reverse = True) 
-    box_count = int(len(score) / m)
-    boxs = []
+    box_count = int(len(score) / m) # 박스 개수
+    boxs = [] # 박스들의 정보
 
-    for i in range(0, box_count): 
+    for i in range(0, box_count): # 박스에 m개의 사과 넣기
         boxs.append(score[i * m : m * (i + 1)]) 
 
-    for box in boxs:
+    for box in boxs: # 박스들을 탐색하면서 판매액 누적
         minimum = box[-1]
-        answer += minimum * len(box)
-
-
-    for i in sorted(score, reverse=True):
-        print(i)
-        
+        answer += minimum * len(box) 
     return answer
 
 
--- programmers/Lv1/명예의전당.py
+++ programmers/Lv1/명예의전당.py
@@ -1,26 +1,45 @@
-# 출연한 가수의 점수가 지금까지 출연 가수들의 점수 중 상위 k번째 이내이면 해당 가수의 점수를 명예의 전당에 올린다.
-# 즉, 프로그램 시작 이후 초기에 k일까지는 모든 출연 가수의 점수가 명예의 전당에 오른다.
-# k일 이후에는 위의 조건에 따라 전당에 올리고, 기존의 마지막 k번째의 순위는 내려가게 된다.
+# # 출연한 가수의 점수가 지금까지 출연 가수들의 점수 중 상위 k번째 이내이면 해당 가수의 점수를 명예의 전당에 올린다.
+# # 즉, 프로그램 시작 이후 초기에 k일까지는 모든 출연 가수의 점수가 명예의 전당에 오른다.
+# # k일 이후에는 위의 조건에 따라 전당에 올리고, 기존의 마지막 k번째의 순위는 내려가게 된다.
 
-# 매일 발표된 명예의 전당의 최하위 점수를 return
+# # 매일 발표된 명예의 전당의 최하위 점수를 return
+
+
+# def solution(k, score):
+#     rank_board = []
+#     report = []
+
+#     for i in range(1, len(score) + 1):
+#         rank_board.append(score[i - 1])
+
+#         if len(rank_board) > k: # k일 이후,
+
+#             rank_board.sort(reverse=True)
+#             del rank_board[-1]
+#             report.append(rank_board[-1])
+
+#         else: # 초기에 k일까지,
+
+#             rank_board.sort(reverse=True)
+#             report.append(rank_board[-1])
+
+#     return report
+
+# solution(3, [10, 100, 20, 150, 1, 100, 200])
 
 
 def solution(k, score):
-    answer = []
     rank_board = []
-    report = []
-    for i in range(1, len(score) + 1): # 1, 2, 3, 4, 5, 6, 7
-        rank_board.append(score[i - 1])
+    answer = []
+
+    for s in score:
+        rank_board.append(s)
 
         if len(rank_board) > k:
-            rank_board.sort(reverse=True)
-            del rank_board[-1]
-            report.append(rank_board[-1])
-            print(report)
-        else:
-            rank_board.sort(reverse=True)
-            report.append(rank_board[-1])
-            print("초기에 k일까지", report)
+            rank_board.remove(min(rank_board))
+            
+        answer.append(min(rank_board))
+
     return answer
 
-solution(3, [10, 100, 20, 150, 1, 100, 200])
+print(solution(3, [10, 100, 20, 150, 1, 100, 200]))
